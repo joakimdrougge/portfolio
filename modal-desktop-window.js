8aed99e56018124b22def9d7e72d6d4e4c58b3d6
Joakim Drougge <joak2384@stud.kea.dk>
2024-02-26 14:41:53 +0100

    Blablabla

--- modal-desktop-window.js
+++ modal-desktop-window.js
@@ -2,10 +2,15 @@ var modal = document.getElementById("modal");
 
 var btn = document.getElementById("about-icon");
 
-var closeButton = document.getElementById("x-button");
+var xButton = document.getElementById("x-button");
+var closeButton = document.getElementById("close-button");
 
 btn.onclick = function () {
     modal.style.display = "grid";
+}
+
+xButton.onclick = function () {
+    modal.style.display = "none";
 }
 
 closeButton.onclick = function () {
