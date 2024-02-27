23e4bfbff92e49872047932f8db0e445cbe6a695
Joakim Drougge <joak2384@stud.kea.dk>
2024-02-27 18:43:20 +0100

    ikoner och grejjer

--- modal-desktop-window.js
+++ modal-desktop-window.js
@@ -1,11 +1,16 @@
 var modal = document.getElementById("modal");
 
 var btn = document.getElementById("portfolio-icon");
+var btn2 = document.getElementById("bin-icon");
 
 var xButton = document.getElementById("x-button");
 var closeButton = document.getElementById("close-button");
 
 btn.onclick = function () {
+    modal.style.display = "grid";
+}
+
+btn2.onclick = function () {
     modal.style.display = "grid";
 }
 
