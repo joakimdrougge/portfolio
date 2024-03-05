c9bce9f27bbe1d975e2c018c54229f79621e533e
Joakim Drougge <joak2384@stud.kea.dk>
2024-03-05 12:26:27 +0100

    wörk wörk

--- modal-desktop-window.js
+++ modal-desktop-window.js
@@ -1,23 +1,102 @@
-var modal = document.getElementById("modal");
+/* About window */
 
-var btn = document.getElementById("portfolio-icon");
-var btn2 = document.getElementById("bin-icon");
+var aboutModal = document.getElementById("about-modal");
+var aboutBtn = document.getElementById("about-icon");
 
-var xButton = document.getElementById("x-button");
+
+var xButton = document.getElementById("about-x-button");
 var closeButton = document.getElementById("close-button");
 
-btn.onclick = function () {
-    modal.style.display = "grid";
-}
-
-btn2.onclick = function () {
-    modal.style.display = "grid";
+aboutBtn.ondblclick = function () {
+    aboutModal.style.display = "grid";
 }
 
 xButton.onclick = function () {
-    modal.style.display = "none";
+    aboutModal.style.display = "none";
 }
 
 closeButton.onclick = function () {
-    modal.style.display = "none";
+    aboutModal.style.display = "none";
+}
+
+/* Desktop window */
+
+var windowModal = document.getElementById("window-modal");
+var binBtn = document.getElementById("bin-icon");
+var portfolioBtn = document.getElementById("portfolio-icon");
+
+var xButton = document.getElementById("window-x-button");
+
+var headerText = document.getElementById("window-header-text");
+
+headerText.innerHTML = "FIX THIS";
+
+binBtn.ondblclick = function () {
+    windowModal.style.display = "grid";
+}
+
+portfolioBtn.ondblclick = function () {
+    windowModal.style.display = "grid";
+}
+
+xButton.onclick = function () {
+    windowModal.style.display = "none";
+}
+
+/* Resumé */
+
+var resume = document.getElementById("resume-container");
+var resumeBtn = document.getElementById("resume-icon");
+var resumeImg = document.getElementById("resume-image");
+
+resumeBtn.ondblclick = function () {
+    resume.style.display = "block";
+}
+
+resumeImg.onclick = function () {
+    resume.style.display = "none";
+}
+
+
+/* Poster */
+
+var poster = document.getElementById("poster-container");
+var posterBtn = document.getElementById("poster-icon");
+var posterImg = document.getElementById("poster-image");
+
+posterBtn.ondblclick = function () {
+    poster.style.display = "block";
+}
+
+posterImg.onclick = function () {
+    poster.style.display = "none";
+}
+
+
+/* Video */
+
+var video = document.getElementById("video-container");
+var videoBtn = document.getElementById("video-icon");
+var videoImg = document.getElementById("dotd-video");
+
+videoBtn.ondblclick = function () {
+    video.style.display = "grid";
+}
+
+var xButton = document.getElementById("video-x-button");
+
+xButton.onclick = function () {
+    video.style.display = "none";
+}
+
+
+/* Show logo text */
+
+function toggleText() {
+    var logoText = document.getElementById("logo-text");
+    if (logoText.style.display === "none") {
+        logoText.style.display = "block";
+    } else {
+        logoText.style.display = "none";
+    }
 }
