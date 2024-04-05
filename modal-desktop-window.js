7c36697de7b20e62615a385a64bfb5a4f39226aa
joakimdrougge <joak2384@stud.kea.dk>
2024-04-05 15:56:57 +0200

    root colours, code cleanup

--- modal-desktop-window.js
+++ modal-desktop-window.js
@@ -1,126 +1,133 @@
-/* Close mobile overlay disclaimer */
+document.addEventListener('DOMContentLoaded', function toggleText() {
 
-document.addEventListener('click', function (event) {
-    var overlay = document.getElementById("overlay");
-    var overlayText = document.getElementById("overlay-text");
+    /* Close mobile overlay disclaimer */
 
-    if (event.target === overlay) {
-        overlay.style.display = "none";
-        overlayText.style.display = "none"
+    document.addEventListener('click', function (event) {
+        var overlay = document.getElementById("overlay");
+        var overlayText = document.getElementById("overlay-text");
+
+        if (event.target === overlay) {
+            overlay.style.display = "none";
+            overlayText.style.display = "none"
+        }
+    });
+
+
+    /* About window */
+
+    var aboutModal = document.getElementById("about-modal");
+    var aboutBtn = document.getElementById("about-icon");
+
+
+    var xButton = document.getElementById("about-x-button");
+    var closeButton = document.getElementById("close-button");
+
+    aboutBtn.ondblclick = function () {
+        aboutModal.style.display = "grid";
     }
-});
 
-/* About window */
+    xButton.onclick = function () {
+        aboutModal.style.display = "none";
+    }
 
-var aboutModal = document.getElementById("about-modal");
-var aboutBtn = document.getElementById("about-icon");
+    closeButton.onclick = function () {
+        aboutModal.style.display = "none";
+    }
 
+    /* Desktop window */
 
-var xButton = document.getElementById("about-x-button");
-var closeButton = document.getElementById("close-button");
+    var windowModal = document.getElementById("portfolio-modal");
+    var portfolioBtn = document.getElementById("portfolio-icon");
 
-aboutBtn.ondblclick = function () {
-    aboutModal.style.display = "grid";
-}
+    var xButton = document.getElementById("window-x-button");
 
-xButton.onclick = function () {
-    aboutModal.style.display = "none";
-}
+    portfolioBtn.ondblclick = function () {
+        windowModal.style.display = "grid";
+    }
 
-closeButton.onclick = function () {
-    aboutModal.style.display = "none";
-}
+    xButton.onclick = function () {
+        windowModal.style.display = "none";
+    }
 
-/* Desktop window */
+    /* Bin window */
 
-var windowModal = document.getElementById("portfolio-modal");
-var portfolioBtn = document.getElementById("portfolio-icon");
+    var binModal = document.getElementById("bin-modal");
+    var binBtn = document.getElementById("bin-icon");
 
-var xButton = document.getElementById("window-x-button");
+    var xButton = document.getElementById("bin-x-button");
 
-portfolioBtn.ondblclick = function () {
-    windowModal.style.display = "grid";
-}
+    binBtn.ondblclick = function () {
+        binModal.style.display = "grid";
+    }
 
-xButton.onclick = function () {
-    windowModal.style.display = "none";
-}
+    xButton.onclick = function () {
+        binModal.style.display = "none";
+    }
 
-/* Bin window */
+    /* Resumé */
 
-var binModal = document.getElementById("bin-modal");
-var binBtn = document.getElementById("bin-icon");
+    var resume = document.getElementById("resume-container");
+    var resumeBtn = document.getElementById("resume-icon");
+    var resumeImg = document.getElementById("resume-image");
 
-var xButton = document.getElementById("bin-x-button");
+    resumeBtn.ondblclick = function () {
+        resume.style.display = "flex";
+    }
 
-binBtn.ondblclick = function () {
-    binModal.style.display = "grid";
-}
+    var xButton = document.getElementById("resume-x-button");
 
-xButton.onclick = function () {
-    binModal.style.display = "none";
-}
-
-/* Resumé */
-
-var resume = document.getElementById("resume-container");
-var resumeBtn = document.getElementById("resume-icon");
-var resumeImg = document.getElementById("resume-image");
-
-resumeBtn.ondblclick = function () {
-    resume.style.display = "flex";
-}
-
-var xButton = document.getElementById("resume-x-button");
-
-xButton.onclick = function () {
-    resume.style.display = "none";
-}
+    xButton.onclick = function () {
+        resume.style.display = "none";
+    }
 
 
-/* Poster */
+    /* Poster */
 
-var poster = document.getElementById("poster-container");
-var posterBtn = document.getElementById("poster-icon");
+    var poster = document.getElementById("poster-container");
+    var posterBtn = document.getElementById("poster-icon");
 
-posterBtn.ondblclick = function () {
-    poster.style.display = "flex";
-}
+    posterBtn.ondblclick = function () {
+        poster.style.display = "flex";
+    }
 
-var xButton = document.getElementById("poster-x-button");
+    var xButton = document.getElementById("poster-x-button");
 
-xButton.onclick = function () {
-    poster.style.display = "none";
-}
-
-
-/* Video */
-
-var video = document.getElementById("video-container");
-var videoBtn = document.getElementById("video-icon");
-var videoImg = document.getElementById("dotd-video");
-
-videoBtn.ondblclick = function () {
-    video.style.display = "flex";
-}
-
-var xButton = document.getElementById("video-x-button");
-
-xButton.onclick = function () {
-    video.style.display = "none";
-}
+    xButton.onclick = function () {
+        poster.style.display = "none";
+    }
 
 
-/* Show logo text */
+    /* Video */
 
-function toggleText() {
+    var video = document.getElementById("video-container");
+    var videoBtn = document.getElementById("video-icon");
+    var videoImg = document.getElementById("dotd-video");
+
+    videoBtn.ondblclick = function () {
+        video.style.display = "flex";
+    }
+
+    var xButton = document.getElementById("video-x-button");
+
+    xButton.onclick = function () {
+        video.style.display = "none";
+    }
+
+
+    var logoImage = document.getElementById("logo-image");
     var logoText = document.getElementById("logo-text");
-    if (logoText.style.display === "none") {
-        logoText.style.display = "block";
-    } else {
-        logoText.style.display = "none";
+
+    logoImage.addEventListener("click", toggleText);
+
+    function toggleText() {
+        if (logoText.style.display === "block") {
+            logoText.style.display = "none";
+        } else {
+            logoText.style.display = "block";
+        }
     }
-}
+
+})
 
 /* Drop-up start menu */
 
@@ -129,7 +136,7 @@ function toggleDropup() {
     dropdown.classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
+/* Close the dropdown menu if the user clicks outside of it */
 window.onclick = function (event) {
     if (!event.target.matches('#start-button')) {
         var dropups = document.getElementsByClassName("drop-up-content");
